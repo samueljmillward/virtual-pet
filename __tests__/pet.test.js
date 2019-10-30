142914f4567add0b3b290cb07679f12f491f2ad1
samueljmillward <samuel.j.pearce@hotmail.com>
2019-10-30 12:01:56 +0000

    aging

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,13 +1,23 @@
 const Pet = require('../src/pet');
 describe('constructor', () => {
     it('returns an object', () => {
-      expect(new Pet('Fido')).toBeInstanceOf(Object);
+        expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
     it('sets the name property', () => {
-      const pet = new Pet('Fido');
-  
-      expect(pet.name).toEqual('Fido');
-    });
-});
+        const pet = new Pet('Fido');
 
-  
+        expect(pet.name).toEqual('Fido');
+    });
+    it('has a initial age of 0', () => {
+        const pet = new Pet('Fido');
+
+        expect(pet.age).toEqual(0);
+    });
+    it('increments the age by 1', () => {
+        const pet = new Pet('Fido');
+      
+        pet.growUp();
+      
+        expect(pet.age).toEqual(1);
+        });
+});
--- src/pet.js
+++ src/pet.js
@@ -1,5 +1,8 @@
 function Pet(name) {
     this.name = name;
+    this.age = 0;
+    this.growUp = function() {
+        this.age += 1;
+ };
 }
-
 module.exports = Pet;
