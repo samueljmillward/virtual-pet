95b8390dde6a0987f3760fa6750e433ba483d496
samueljmillward <samuel.j.pearce@hotmail.com>
2019-10-30 14:40:17 +0000

    keeping fit

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -20,4 +20,44 @@ describe('constructor', () => {
       
         expect(pet.age).toEqual(1);
         });
+    it('has a initial hunger of 0', () => {
+        const pet = new Pet ('Fido');
+
+        expect(pet.hunger).toEqual(0);
+    });
+    it('has a initial fitness of 10', () => {
+        const pet = new Pet ('Fido');
+
+        expect(pet.fitness).toEqual(10);
+    });
 });
+
+describe('growUp', () => {
+    it('increases age/hunger/fitness by n', () => {
+        const pet = new Pet ('Fido');
+
+        pet.growUp();
+        expect(pet.age).toEqual(1);
+        expect(pet.hunger).toEqual(5);
+        expect(pet.fitness).toEqual(7);
+    });
+});
+
+describe('walk', () => {
+    it('increases fitness by 4', () => {
+      const pet = new Pet('Fido');
+  
+      pet.fitness = 4;
+      pet.walk();
+  
+      expect(pet.fitness).toEqual(8);
+    });
+    it('increases fitness by to a maximum of 10', () => {
+        const pet = new Pet('Fido');
+    
+        pet.fitness = 8;
+        pet.walk();
+    
+        expect(pet.fitness).toEqual(10);
+      });
+  });
--- src/pet.js
+++ src/pet.js
@@ -1,8 +1,22 @@
+const MAXIMUM_FITNESS = 10;
 function Pet(name) {
     this.name = name;
     this.age = 0;
-    this.growUp = function() {
-        this.age += 1;
- };
-}
+    this.hunger = 0;
+    this.fitness = MAXIMUM_FITNESS;
+};
+
+Pet.prototype.growUp = function () {
+    this.age += 1;
+    this.hunger += 5;
+    this.fitness -= 3;
+};
+
+Pet.prototype.walk = function() {
+    if ((this.fitness + 4) <= 10 ) {
+      this.fitness += 4;
+    } else {
+      this.fitness = 10;
+    }
+  }
 module.exports = Pet;
